3b8b7da7df58aa26ea7f47d6eb82f53d6bb3213d
Paul Butler <paul@driftingin.space>
2023-01-31 21:56:13 +0000

    fix mouse up issue

--- js-pkg/demos/src/pages/voxel.tsx
+++ js-pkg/demos/src/pages/voxel.tsx
@@ -6,6 +6,8 @@ import { useCallback, useRef, useState } from 'react'
 import { Vector3, Vector3Tuple } from 'three'
 import { CompactPicker } from 'react-color'
 
+const DIM = 15
+
 interface Voxel {
     position: Vector3Tuple
     color: any
@@ -29,8 +31,8 @@ function Voxel(props: { voxel: Voxel, name?: string }) {
 
 function getPosition(event: ThreeEvent<PointerEvent>): Vector3Tuple | null {
     if (event.intersections.length === 0) return null
-    
-    const {face, point} = event.intersections[0]
+
+    const { face, point } = event.intersections[0]
     const normal: Vector3 = face!.normal.clone()
 
     const pos: Vector3 = point.clone().add(new Vector3(0.5, 0.0, 0.5))
@@ -66,15 +68,25 @@ function voxelReducer(state: Record<string, Voxel>, action: VoxelAction): Record
     return state
 }
 
+interface VoxelSetProps {
+    voxels: Record<string, Voxel>
+}
+
+function VoxelSet(props: VoxelSetProps) {
+    return <>{
+        Object.entries(props.voxels).map(([index, voxel]) => <Voxel key={index} voxel={voxel} name={index} />)
+    }</>
+}
+
 export function VoxelEditor() {
     const [ghostPosition, setGhostPosition] = useState<[number, number, number] | null>(null)
     const [voxels, dispatch] = useSharedReducer("voxels", voxelReducer, [] as any)
     const [color, setColor] = useState('#D33115')
-
+    
     const positionHasBeenSet = useRef(false)
     const setInitialCameraPosition = (controls: any) => {
         if (controls && !positionHasBeenSet.current) {
-            controls.object.position.set(0, 10, 10)
+            controls.object.position.set(0, DIM, DIM)
             positionHasBeenSet.current = true
         }
     }
@@ -83,12 +95,17 @@ export function VoxelEditor() {
         position: ghostPosition,
         color: color
     })
-    
+
     const pointerMove = useCallback((event: ThreeEvent<PointerEvent>) => {
         setGhostPosition(getPosition(event))
     }, [setGhostPosition])
 
     const handleClick = useCallback((event: ThreeEvent<MouseEvent>) => {
+        if (event.delta > 5) {
+            // ignore drag events, which are handled by the orbit control
+            return
+        }
+
         if (event.shiftKey) {
             if (event.object.name) {
                 dispatch({
@@ -112,43 +129,41 @@ export function VoxelEditor() {
 
     return (
         <>
-        <div style={{ position: 'absolute', top: 0, right: 0, left: 0, bottom: 0 }}>
-            <Canvas>
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                <pointLight position={[-10, -10, -10]} />
-
-                {
-                    ghostPosition ? <Voxel voxel={{ position: ghostPosition, color: 0x000000, opacity: 0.5 }} /> : null
-                }
-
-                {
-                    Object.entries(presence).map(([id, user]) => {
-                        if (user.position === null) return null
-
-                        return <Voxel key={id} voxel={{ position: user.position, color: user.color, opacity: 0.5 }} />
-                    })
-                }
-                
-                <gridHelper args={[11, 11]} position={[0, 0.001, 0]} />
-
-                <group onPointerMove={pointerMove} onClick={handleClick}>
-                    <mesh
-                        scale={1}
-                        position={[0, -0.05, 0]}
-                    >
-                        <boxGeometry args={[11, 0.1, 11]} />
-                        <meshStandardMaterial color="#eee" opacity={1} />
-                    </mesh>
+            <div style={{ position: 'absolute', top: 0, right: 0, left: 0, bottom: 0 }}>
+                <Canvas>
+                    <OrbitControls ref={setInitialCameraPosition} />
+                    <ambientLight intensity={0.5} />
+                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                    <pointLight position={[-10, -10, -10]} />
 
                     {
-                        Object.entries(voxels).map(([index, voxel]) => <Voxel key={index} voxel={voxel} name={index} />)
+                        ghostPosition ? <Voxel voxel={{ position: ghostPosition, color: 0x000000, opacity: 0.5 }} /> : null
                     }
-                </group>
-                <OrbitControls ref={setInitialCameraPosition} />
-            </Canvas>
-        </div>
-        <CompactPicker color={color} onChangeComplete={(color) => setColor(color.hex)} />
+
+                    {
+                        Object.entries(presence).map(([id, user]) => {
+                            if (user.position === null) return null
+
+                            return <Voxel key={id} voxel={{ position: user.position, color: user.color, opacity: 0.5 }} />
+                        })
+                    }
+
+                    <gridHelper args={[DIM, DIM]} position={[0, 0.001, 0]} />
+
+                    <group onPointerMove={pointerMove} onClick={handleClick}>
+                        <mesh
+                            scale={1}
+                            position={[0, -0.05, 0]}
+                        >
+                            <boxGeometry args={[DIM, 0.1, DIM]} />
+                            <meshStandardMaterial color="#eee" opacity={1} />
+                        </mesh>
+                        <VoxelSet voxels={voxels} />
+                    </group>
+
+                </Canvas>
+            </div>
+            <CompactPicker color={color} onChangeComplete={(color) => setColor(color.hex)} />
         </>
     )
 }
@@ -157,6 +172,6 @@ export default function App() {
     return (
         <DriftDBProvider api={DRIFTDB_URL}>
             <VoxelEditor />
-        </DriftDBProvider>        
+        </DriftDBProvider>
     )
 }
