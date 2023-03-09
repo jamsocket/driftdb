dcf4da2904b7582ea200ff4033cb5922ce1413be
Paul Butler <paul@driftingin.space>
2023-03-09 19:00:19 +0000

    cleanup

--- js-pkg/packages/driftdb/src/http.d.ts
+++
@@ -1,7 +0,0 @@
-import { MessageFromDb, MessageToDb } from "./types";
-export declare class HttpConnection {
-    private httpUrl;
-    constructor(httpUrl: string);
-    send(message: MessageToDb): Promise<MessageFromDb>;
-}
-//# sourceMappingURL=http.d.ts.map
