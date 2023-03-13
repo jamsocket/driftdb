d836d76264f80647f868bdd40482885fb8a2dcb4
Paul Butler <paul@driftingin.space>
2023-03-13 14:50:16 +0000

    crdt->useBinary

--- js-pkg/apps/demos/src/pages/state.tsx
+++ js-pkg/apps/demos/src/pages/state.tsx
@@ -67,7 +67,7 @@ export default function Demos() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div>
-        <DriftDBProvider api={DRIFTDB_URL} crdt={true}>
+        <DriftDBProvider api={DRIFTDB_URL} useBinary={true}>
           <h1 className="text-2xl font-bold text-gray-800 mb-6">DriftDB - Shared State Demo</h1>
           <StateDemo />
           <div className="flex flex-col gap-4 sm:max-w-sm border border-gray-300 bg-gray-200 p-6 rounded-3xl">
--- js-pkg/apps/demos/src/pages/tictactoe.tsx
+++ js-pkg/apps/demos/src/pages/tictactoe.tsx
@@ -248,7 +248,7 @@ export default function TicTacToe() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div>
-        <DriftDBProvider api={DRIFTDB_URL} crdt={true}>
+        <DriftDBProvider api={DRIFTDB_URL} useBinary={true}>
           <h1 className="text-2xl font-bold text-gray-800">DriftDB - Tic Tac Toe Demo</h1>
           <TicTacToeDemo />
           <div className="flex flex-col gap-4 sm:max-w-sm border border-gray-300 bg-gray-200 p-6 rounded-3xl">
