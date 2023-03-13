fb2b22fc01b7c8fe5325ebadd8fa798f5675a819
Abhishek Cherath <abhi@quic.us>
2023-03-13 16:54:29 -0400

    capitalize description

--- js-pkg/apps/demos/src/pages/index.tsx
+++ js-pkg/apps/demos/src/pages/index.tsx
@@ -31,7 +31,7 @@ const demos = [
   {
     title: 'WebRTC Chat',
     href: '/webrtc-demo',
-    description: 'simple chat with webrtc datachannels, using driftdb for signaling'
+    description: 'Simple chat with WebRTC datachannels, using DriftDB for signaling'
   }
 ]
 
