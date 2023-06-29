d623af9aedb60640631465f31c3f65e68c5cf51e
Paul Butler <paul@driftingin.space>
2023-06-29 14:37:14 -0400

    fix tests

--- test.sh
+++ test.sh
@@ -12,12 +12,4 @@ cargo run &
 
 cd ${BASE_DIR}/js-pkg/packages/driftdb
 npm ci
-npm run build
-
-cd ${BASE_DIR}/js-pkg/packages/driftdb-react
-npm ci
-npm run build
-
-cd ${BASE_DIR}/js-pkg/apps/tests
-npm ci
-npm run test
+npm test
