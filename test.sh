58fde2b30c6e291cae17a4b1abf349613263bd4c
Paul Butler <paul@driftingin.space>
2023-07-01 16:17:24 -0400

    add a wait timeout

--- test.sh
+++ test.sh
@@ -23,8 +23,16 @@ kill ${NATIVE_SERVER_PID}
 cd ${BASE_DIR}/driftdb-worker
 npm ci
 npm run dev &
-
 WORKER_SERVER_PID=$!
+
+# Wait for the server to start. Give up after 30 seconds.
+for i in {1..30}; do
+  if curl -s http://127.0.0.1:8787 > /dev/null; then
+    break
+  fi
+  echo "Waiting for server to start..."
+  sleep 1
+done
 
 cd ${BASE_DIR}/js-pkg/packages/driftdb
 DRIFTDB_API=http://127.0.0.1:8787 npm test -- --forceExit --detectOpenHandles
